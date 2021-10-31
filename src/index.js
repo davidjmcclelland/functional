28edabe8d64795822e76663156db0c9487813a22
MAC McClelland <dmcclelland@ptc.com>
2021-10-31 08:30:29 -0400

    jsNode self-serve es6 project starter

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+    res.end('Hello from the server');
+}).listen(4001);
+const sayHello = name => `Hello ${name}`;
+
+console.log(sayHello('srcDave'));
+console.log('Server is up and running');
+
+export default server;
