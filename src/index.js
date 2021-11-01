2c1e0b9ab06ead932eb2e96aa75874df342024e7
MAC McClelland <dmcclelland@ptc.com>
2021-11-01 09:52:17 -0400

    es6 function examples added to index.js

--- src/index.js
+++ src/index.js
@@ -14,3 +14,30 @@ const person = {
     age: 37
 }
 person.name = "mary";
+
+// es6 function transformation
+// regular old function
+const oldFunction = function(arg1, arg2){
+    return arg1 + arg2;
+}
+
+// 1:1 change to es6
+const newFunction = (arg1, arg2) => {
+    return arg1 + arg2;
+}
+
+// inlined es6 function
+
+const inlineFunction = (arg1, arg2) => arg1 + arg2;
+
+// inlined no args
+
+const noArgs = () => console.log('noArgs');
+
+// when returning an object you must wrap in parens
+
+const getSomeData = () => ({
+    name: 'John Doe',
+    age: 90,
+    job: 'Bug Factory'
+});
